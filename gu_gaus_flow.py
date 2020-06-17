d5d227f65b3e59d97f779e278617ad48bd915a8e
lliutianc <lliuitanc@gmail.com>
2020-06-16 23:27:12 -0400

    use auto pep8 to reformat codes.

--- gu_gaus_flow.py
+++ gu_gaus_flow.py
@@ -1,3 +1,23 @@
+import pdb
+from gu import *
+from util import *
+from Gaussianization_Flows.models.flow_model import Net
+import seaborn.apionly as sns
+import matplotlib.pyplot as plt
+import matplotlib
+import datasets
+import torchvision.transforms as transforms
+from torchvision.utils import save_image, make_grid
+import torchvision
+from torch.utils.data import DataLoader, Subset
+import torch.distributions as tdist
+import torch.optim as optim
+import torch.nn.init as init
+import torch.nn.functional as F
+import torch.nn as nn
+import torch.utils.data
+import torch
+import numpy as np
 import os
 import sys
 import math
@@ -11,63 +31,98 @@ curPath = os.path.abspath(os.path.dirname(__file__))
 rootPath = os.path.split(curPath)[0]
 sys.path.append(curPath)
 
-import numpy as np
-import torch
-import torch.utils.data
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.nn.init as init
-import torch.optim as optim
-import torch.distributions as tdist
-from torch.utils.data import DataLoader, Subset
 
-import torchvision
-from torchvision.utils import save_image, make_grid
-import torchvision.transforms as transforms
-import datasets
-
-import matplotlib
 matplotlib.use('Agg')
-import matplotlib.pyplot as plt
-import seaborn.apionly as sns
 plt.style.use('seaborn-paper')
 
-from Gaussianization_Flows.models.flow_model import Net
-from util import *
-from gu import *
-import pdb
 
 # add device
 
 parser = argparse.ArgumentParser()
 # data
-parser.add_argument('--gu_num', type=int, default=8, help='Components of GU clusters.')
+parser.add_argument('--gu_num', type=int, default=8,
+                    help='Components of GU clusters.')
 parser.add_argument('--dataset', default='GU', help='Which dataset to use.')
 parser.add_argument('--seed', type=int, default=1, help='Random seed to use.')
 
 # model parameters
-parser.add_argument('--input_size', type=int, default=1, help='Input size in a model.')
-parser.add_argument('--layer', type=int, default=5, help="Total number of Gaussianization layers")
-parser.add_argument('--kde_num', type=int, default=1, help='Stacking multiple KDE layers before each rotation layer')
-parser.add_argument('--total_datapoints', type=int, default=100, help="Total number of data points for each KDE layer")
-parser.add_argument('--usehouseholder', action='store_true', help='Train rotation matrix using householder reflection')
-parser.add_argument('--multidim_kernel', action='store_true', help='Use multiple dimension bandwidth kernel')
+parser.add_argument(
+    '--input_size',
+    type=int,
+    default=1,
+    help='Input size in a model.')
+parser.add_argument(
+    '--layer',
+    type=int,
+    default=5,
+    help="Total number of Gaussianization layers")
+parser.add_argument(
+    '--kde_num',
+    type=int,
+    default=1,
+    help='Stacking multiple KDE layers before each rotation layer')
+parser.add_argument(
+    '--total_datapoints',
+    type=int,
+    default=100,
+    help="Total number of data points for each KDE layer")
+parser.add_argument(
+    '--usehouseholder',
+    action='store_true',
+    help='Train rotation matrix using householder reflection')
+parser.add_argument(
+    '--multidim_kernel',
+    action='store_true',
+    help='Use multiple dimension bandwidth kernel')
 
 parser.add_argument('--early_stopping', type=int, default=10)
 # training params
-parser.add_argument('--batch_size', type=int, default=2048, help='Batch size in training.')
-parser.add_argument('--niters', type=int, default=50000, help='Total iteration numbers in training.')
+parser.add_argument(
+    '--batch_size',
+    type=int,
+    default=2048,
+    help='Batch size in training.')
+parser.add_argument('--niters', type=int, default=50000,
+                    help='Total iteration numbers in training.')
 parser.add_argument('--lr', type=float, default=1e-2, help='Learning rate.')
-parser.add_argument('--weight_decay', type=float, default=0, help='Weight decay in Adam.')
+parser.add_argument(
+    '--weight_decay',
+    type=float,
+    default=0,
+    help='Weight decay in Adam.')
 parser.add_argument('--beta1', type=float, default=0.9, help='Beta 1 in Adam.')
-parser.add_argument('--beta2', type=float, default=0.999, help='Beta 2 in Adam.')
-parser.add_argument('--process_size', type=int, default=100, help="Process size")
-parser.add_argument('--adjust_step', type=int, default=10000, help="Decrease learning rate after a couple steps")
+parser.add_argument(
+    '--beta2',
+    type=float,
+    default=0.999,
+    help='Beta 2 in Adam.')
+parser.add_argument(
+    '--process_size',
+    type=int,
+    default=100,
+    help="Process size")
+parser.add_argument(
+    '--adjust_step',
+    type=int,
+    default=10000,
+    help="Decrease learning rate after a couple steps")
 
-parser.add_argument('--log_interval', type=int, default=1000, help='How often to show loss statistics and save models/samples.')
+parser.add_argument(
+    '--log_interval',
+    type=int,
+    default=1000,
+    help='How often to show loss statistics and save models/samples.')
 
-parser.add_argument('--cuda', type=int, default=0, help='Number of CUDA to use if available.')
-parser.add_argument('--eval_size', type=int, default=100000, help='Sample size in evaluation.')
+parser.add_argument(
+    '--cuda',
+    type=int,
+    default=0,
+    help='Number of CUDA to use if available.')
+parser.add_argument(
+    '--eval_size',
+    type=int,
+    default=100000,
+    help='Sample size in evaluation.')
 
 
 def flow_loss(u, log_jacob, size_average=True):
@@ -115,11 +170,16 @@ def train(model, dataloader, optimizer, args):
 
                 real = dataloader.get_sample(args.eval_size)
                 prior = model.base_dist.sample((args.eval_size,))
-                fake = model.sampling(DATA, prior, process_size=args.eval_size, sample_num=args.eval_size)
+                fake = model.sampling(
+                    DATA,
+                    prior,
+                    process_size=args.eval_size,
+                    sample_num=args.eval_size)
                 w_distance_real = w_distance(real, fake)
 
-                logger.info(f'Iter {i} / {args.niters}, Time {round(time.time() - start, 4)},  '
-                            f'w_distance_real: {w_distance_real}, loss {round(running_loss / args.log_interval, 5)}')
+                logger.info(
+                    f'Iter {i} / {args.niters}, Time {round(time.time() - start, 4)},  '
+                    f'w_distance_real: {w_distance_real}, loss {round(running_loss / args.log_interval, 5)}')
 
                 real_sample = real.cpu().data.numpy().squeeze()
                 fake_sample = fake.cpu().data.numpy().squeeze()
@@ -140,17 +200,36 @@ def train(model, dataloader, optimizer, args):
                 # _sample = np.concatenate([real_sample, fake_sample])
                 kde_num = 200
                 min_real, max_real = min(real_sample), max(real_sample)
-                kde_width_real = kde_num * (max_real - min_real) / args.eval_size
+                kde_width_real = kde_num * \
+                    (max_real - min_real) / args.eval_size
                 min_fake, max_fake = min(fake_sample), max(fake_sample)
-                kde_width_fake = kde_num * (max_fake - min_fake) / args.eval_size
-                sns.kdeplot(real_sample, bw=kde_width_real, label='Data', color='green', shade=True, linewidth=6)
-                sns.kdeplot(fake_sample, bw=kde_width_fake, label='Model', color='orange', shade=True, linewidth=6)
+                kde_width_fake = kde_num * \
+                    (max_fake - min_fake) / args.eval_size
+                sns.kdeplot(
+                    real_sample,
+                    bw=kde_width_real,
+                    label='Data',
+                    color='green',
+                    shade=True,
+                    linewidth=6)
+                sns.kdeplot(
+                    fake_sample,
+                    bw=kde_width_fake,
+                    label='Model',
+                    color='orange',
+                    shade=True,
+                    linewidth=6)
 
-                ax.set_title(f'True EM Distance: {w_distance_real}.', fontsize=FONTSIZE)
+                ax.set_title(
+                    f'True EM Distance: {w_distance_real}.',
+                    fontsize=FONTSIZE)
                 ax.legend(loc=2, fontsize=FONTSIZE)
                 ax.set_ylabel('Estimated Density by KDE', fontsize=FONTSIZE)
                 ax.tick_params(axis='x', labelsize=FONTSIZE * 0.7)
-                ax.tick_params(axis='y', labelsize=FONTSIZE * 0.5, direction='in')
+                ax.tick_params(
+                    axis='y',
+                    labelsize=FONTSIZE * 0.5,
+                    direction='in')
 
                 cur_img_path = os.path.join(image_path, str(i) + '.jpg')
                 plt.tight_layout()
@@ -192,22 +271,43 @@ if __name__ == '__main__':
 
     # load data
     if args.gu_num == 8:
-        dataloader = GausUniffMixture(n_mixture=args.gu_num, mean_dist=10, sigma=2, unif_intsect=1.5, unif_ratio=1.,
-                                      device=args.device, extend_dim=False)
+        dataloader = GausUniffMixture(
+            n_mixture=args.gu_num,
+            mean_dist=10,
+            sigma=2,
+            unif_intsect=1.5,
+            unif_ratio=1.,
+            device=args.device,
+            extend_dim=False)
     else:
-        dataloader = GausUniffMixture(n_mixture=args.gu_num, mean_dist=5, sigma=0.1, unif_intsect=5, unif_ratio=3,
-                                      device=args.device, extend_dim=False)
+        dataloader = GausUniffMixture(
+            n_mixture=args.gu_num,
+            mean_dist=5,
+            sigma=0.1,
+            unif_intsect=5,
+            unif_ratio=3,
+            device=args.device,
+            extend_dim=False)
     args.input_size = 1
 
     # model
-    model = Net(args.total_datapoints, args.layer, args.input_size, args.kde_num, multidim_kernel=args.multidim_kernel,
-              usehouseholder=args.usehouseholder)
+    model = Net(
+        args.total_datapoints,
+        args.layer,
+        args.input_size,
+        args.kde_num,
+        multidim_kernel=args.multidim_kernel,
+        usehouseholder=args.usehouseholder)
     model = model.to(args.device)
 
-    optimizer = torch.optim.Adam(model.parameters(), lr=args.lr, weight_decay=args.weight_decay, betas=(args.beta1, args.beta2))
+    optimizer = torch.optim.Adam(
+        model.parameters(),
+        lr=args.lr,
+        weight_decay=args.weight_decay,
+        betas=(
+            args.beta1,
+            args.beta2))
 
     logger.info('Start training...')
     train(model, dataloader, optimizer, args)
     logger.info('Finish All...')
-
-
